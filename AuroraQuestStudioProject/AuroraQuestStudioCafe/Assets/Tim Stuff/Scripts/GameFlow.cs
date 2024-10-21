4f4165ac37efad4b7827b3b4e906cc4a920f52f6
Kazwyrm <tlangford203@gmail.com>
2024-10-21 12:06:50 +1100

    adding text and stuff

---
+++ AuroraQuestStudioProject/AuroraQuestStudioCafe/Assets/Tim Stuff/Scripts/GameFlow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFlow : MonoBehaviour
+{
+    Text 
+        currentSentence,
+        nextSentence;
+
+    string
+       line_1 = "",
+       line_2 = "",
+       line_3 = "",
+       line_4 = "",
+       line_5 = "",
+       line_6 = "",
+       line_7 = "",
+       line_8 = "",
+       line_9 = "",
+       line_10 = "";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
